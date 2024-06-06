2a86695ec5a28d507b18da2b2e7a3eedddab2be6
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-06-06 12:31:06 +0500

    url update

--- src/components/pages/payment/payments.jsx
+++ src/components/pages/payment/payments.jsx
@@ -1,46 +1,72 @@
-import axios from "../../../api";
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { FaRegUser } from "react-icons/fa";
+import user from "../../../assets/images/user.jpg";
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const Payments = () => {
   const [cardData, setCardData] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handlePrev = () => {
+    page > 1 ? setPage((page) => page - 1) : <></>;
+  };
+
+  const handleNext = () => setPage((page) => page + 1);
+
   useEffect(() => {
     axios
-      .get("/users")
-      // .then((res) => console.log(res.data.users[0]))
+      .get(`https://dummyjson.com/users?limit=3&skip=${(page - 1) * 3}`)
       .then((res) => setCardData(res.data.users))
       .catch((err) => console.log(err));
-  }, []);
+  }, [page]);
 
   return (
     <>
-      <h1>Users</h1>
-      <h2>nega</h2>
+      <h1 style={{ color: "#252120", marginLeft: 20 }}>Users</h1>
       <div className="container">
         <div className="row">
           {cardData.map((card) => (
             <div className="col-md-4 mb-4" key={card.id}>
               <div className="card">
-                <div
-                  className="card-img-top d-flex justify-content-center align-items-center"
-                  style={{ height: "150px" }}
-                >
-                  <FaRegUser size="3em" />
+                <div>
+                  <img
+                    src={user}
+                    alt="User_photo"
+                    style={{
+                      width: "100%",
+                      outline: "hidden",
+                      borderRadius: 6,
+                    }}
+                  />
                 </div>
                 <div className="card-body">
                   <h5 className="card-title">
-                    FullName:
+                    <small>FullName</small>: <br />
                     {card.firstName} {card.lastName}
                   </h5>
+
                   <p className="card-text"> Phone: {card.phone}</p>
+                  <h6> City: {card.address.city}</h6>
                   <a href="#" className="btn btn-primary">
-                    Go somewhere
+                    More
                   </a>
                 </div>
               </div>
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="d-flex justify-content-end align-items-center gap-2 mr-4">
+        <button className="btn btn-outline-dark" onClick={handlePrev}>
+          orqaga
+        </button>
+        <h2>{page}</h2>
+        <button className="btn btn-outline-success" onClick={handleNext}>
+          keyingisi
+        </button>
       </div>
     </>
   );
